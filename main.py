0a43d1a8cd9c155501121902dd41462085a4c865
Minh Dương <i-am@maskduck.ninja>
2022-02-10 20:26:10 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from threading import Thread
+import nextcord
+from flask import jsonify
+from nextcord.ext import commands
+intents = nextcord.Intents.all()
+bot = commands.Bot(command_prefix = '?')
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+@app.route('/vscode/<user_id>')
+def vscode(user_id):
+    member = bot.get_guild(941320065444360242).get_member(int(user_id))
+    if member.activity.name.lower() == 'playing visual studio code':
+        return jsonify(
+            {
+                "schemaVersion": 1,
+                "label": "coding",
+                "message": member.activity.state.split(': ')[1],
+                "color": "blue"
+            }
+        )
+    
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+@bot.command()
+async def henlo(ctx):
+    await ctx.send('hi')
+
+keep_alive()
+bot.run('')
